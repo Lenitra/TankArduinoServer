c75cc81d8f284d610b2a816c62e72c56e1b2dcf4
Lenitra <lenitramc@gmail.com>
2024-02-28 14:13:43 +0100

    Refactor video streaming server to forward received data to all connected clients

--- SERVEUR/main.py
+++ SERVEUR/main.py
@@ -1,34 +1,27 @@
 import asyncio
 import websockets
-import cv2
-import numpy as np
-
-# Fonction pour décoder les images JPEG binaires
-def decode_image(data):
-    nparr = np.frombuffer(data, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
-
-async def video_stream(websocket, path):
-    print("Client connecté")
-    while True:
-        try:
-            # Recevoir les données binaires de l'image depuis le websocket
-            data = await websocket.recv()
-            # Décoder l'image
-            image = decode_image(data)
-            # Afficher l'image (vous pouvez choisir de la sauvegarder ou la traiter autrement)
-            cv2.imshow("ESP32 Video Stream", image)
-            cv2.waitKey(1)  # Attendre un peu pour la mise à jour de l'affichage
-        except websockets.ConnectionClosedError:
-            print("Client déconnecté")
-            break
 
 # Spécifiez l'adresse IP publique de votre serveur et le port sur lequel le serveur WebSocket écoute
 server_ip = "217.160.99.153"
 server_port = 55055
 
-start_server = websockets.serve(video_stream, server_ip, server_port)
+async def video_stream(websocket, path):
+    print("Client connecté")
+    while True:
+        try:
+            # Recevoir les données binaires de l'image depuis le websocket client
+            data = await websocket.recv()
+            # Réacheminer les données reçues à tous les autres clients connectés
+            for ws in server.websockets:
+                if ws != websocket and ws.open:
+                    await ws.send(data)
+        except websockets.ConnectionClosedError:
+            print("Client déconnecté")
+            break
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def server():
+    async with websockets.serve(video_stream, server_ip, server_port):
+        print(f"Serveur WebSocket démarré sur {server_ip}:{server_port}")
+        await asyncio.Future()  # Garde le serveur en cours d'exécution indéfiniment
+
+asyncio.run(server())
